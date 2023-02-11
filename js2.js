9e4c949a7760d3a15785dcb7c5451e8861c28e96
Isha Kunwar <isha.12011399@lpu.in>
2023-02-11 14:04:33 +0530

    responsive check 1

--- js2.js
+++ js2.js
@@ -3,6 +3,9 @@
     let hill1 = document.getElementById( 'hill1');
     let hill4 = document.getElementById( 'hill4');
     let hill5 = document.getElementById('hill5');
+    let hill2 = document.getElementById('hill2');
+    let hill3 = document.getElementById('hill3');
+    let tree = document.getElementById('tree');
         
         window.addEventListener(
             'scroll', () => {
@@ -13,5 +16,8 @@
                 hill5.style.left = value * 1.05 + 'px';
                 hill4.style.left = value * -1.05 + 'px';
                 hill1.style.top = value * -0.5 + 'px';
+                // hill2.style.top = value * -0.1 + 'px';
+                hill3.style.top = value * -0.1 + 'px';
+                tree.style.right = value * 0.2 + 'px';
             }
         );
